9af1c0ea205a4cf2f7bb640f7ab1c5c4e04688fa
your-name <osherberger@gmail.com>
2024-04-18 10:58:31 +0300

    Elevator

---
+++ index.js
@@ -0,0 +1,242 @@
+// class Floor {
+//   level: number;
+//   constructor(level: number) {
+//     this.level = level;
+//   }
+// }
+// class Elevator {
+//   currentFloor: Floor;
+//   isMoving: boolean;
+//   queue: Floor[];
+//   isWaiting: boolean;
+//   elevatorElement: HTMLElement;
+//   constructor(element: HTMLElement) {
+//     this.currentFloor = new Floor(0);
+//     this.isMoving = false;
+//     this.queue = [];
+//     this.isWaiting = false;
+//     this.elevatorElement = element;
+//   }
+//   move(floor: Floor) {
+//     if (!this.isMoving) {
+//       this.isMoving = true;
+//       const travelDistance = Math.abs(this.currentFloor.level - floor.level); 
+//       const animationDuration = travelDistance * 0.5 * 1000; 
+//       this.elevatorElement.style.setProperty('--animation-duration', `${animationDuration}ms`);
+//       this.elevatorElement.style.setProperty('--target-floor', floor.level.toString());
+//       this.elevatorElement.style.setProperty('--current-floor', this.currentFloor.level.toString());
+//       setTimeout(() => {
+//         this.currentFloor = floor;
+//         console.log(`Elevator arrived at floor ${this.currentFloor.level}`);
+//         this.playSound();
+//         this.updateElevatorPosition();
+//         this.isMoving = false;
+//         if (this.queue.length > 0) {
+//           this.isWaiting = true;
+//           setTimeout(() => {
+//             this.isWaiting = false;
+//             const nextFloor = this.queue.shift();
+//             if (nextFloor) {
+//               this.move(nextFloor);
+//             }
+//           }, 2000); 
+//         }
+//       }, animationDuration);
+//     } else {
+//       this.queue.push(floor);
+//     }
+//     this.elevatorElement.style.setProperty('--target-floor', floor.level.toString());
+//     this.elevatorElement.style.setProperty('--current-floor', this.currentFloor.level.toString());
+//   }
+//   requestFloor(floor: Floor) {
+//     if (!this.isWaiting) {
+//       this.move(floor);
+//     } else {
+//       this.queue.push(floor);
+//     }
+//   }
+//   playSound() {
+//     const audio = new Audio('ding.mp3');
+//     audio.play();
+//     console.log('Ding!');
+//   }
+//   updateElevatorPosition() {
+//     const translateY = `calc(${this.currentFloor.level} * -57px)`;
+//     this.elevatorElement.style.transform = `translateY(${translateY})`;
+//   }
+// }
+// class Building {
+//   numberOfFloors: number;
+//   floorButtonsContainer: HTMLElement;
+//   constructor(numberOfFloors: number, floorButtonsContainer: HTMLElement) {
+//     this.numberOfFloors = numberOfFloors;
+//     this.floorButtonsContainer = floorButtonsContainer;
+//   }
+//   createFloorButtons() {
+//     for (let i = this.numberOfFloors; i >= 0; i--) {
+//       const button = document.createElement('button');
+//       button.classList.add('floor', 'metal', 'linear');
+//       button.innerText = i.toString();
+//       button.addEventListener('click', function() {
+//         requestElevator(new Floor(i));
+//       }.bind(this));
+//       const div = document.createElement('div');
+//       div.classList.add('blackline');
+//       const floorDiv = document.createElement('div');
+//       floorDiv.classList.add('floor');
+//       floorDiv.appendChild(button);
+//       this.floorButtonsContainer.appendChild(div);
+//       this.floorButtonsContainer.appendChild(floorDiv);
+//     }
+//   }
+// }
+// class BuildingFactory {
+//   createBuilding(numberOfFloors: number, floorButtonsContainer: HTMLElement) {
+//     return new Building(numberOfFloors, floorButtonsContainer);
+//   }
+// }
+// const floorButtonsContainer = document.getElementById('floorButtonsContainer');
+// const buildingFactory = new BuildingFactory();
+// let elevator: Elevator | null = null;
+// function requestElevator(floor: Floor) {
+//   if (!elevator) {
+//     elevator = new Elevator(document.querySelector('.elevator img')!);
+//   }
+//   elevator.requestFloor(floor);
+// }
+// const numberOfFloors = 15;
+// const building = buildingFactory.createBuilding(numberOfFloors, floorButtonsContainer!);
+// building.createFloorButtons();
+var Floor = /** @class */ (function () {
+    function Floor(level) {
+        this.level = level;
+    }
+    return Floor;
+}());
+var Elevator = /** @class */ (function () {
+    function Elevator(element) {
+        this.currentFloor = new Floor(0);
+        this.isMoving = false;
+        this.queue = [];
+        this.isWaiting = false;
+        this.elevatorElement = element;
+    }
+    Elevator.prototype.move = function (floor) {
+        var _this = this;
+        if (!this.isMoving) {
+            this.isMoving = true;
+            var currentY = parseInt(getComputedStyle(this.elevatorElement).getPropertyValue('transform').split(',')[5], 10);
+            var targetY = -57 * floor.level;
+            // Calculate the distance and duration of the animation
+            var distance = Math.abs(targetY - currentY);
+            var animationDuration = distance * 5;
+            // Ensure animationDuration is non-negative
+            var duration = Math.max(animationDuration, 0);
+            // Animate the elevator's movement
+            this.animateElevator(currentY, targetY, duration, function () {
+                _this.currentFloor = floor;
+                console.log("Elevator arrived at floor ".concat(_this.currentFloor.level));
+                _this.playSound();
+                _this.updateElevatorPosition();
+                _this.isMoving = false;
+                if (_this.queue.length > 0) {
+                    _this.isWaiting = true;
+                    setTimeout(function () {
+                        _this.isWaiting = false;
+                        var nextFloor = _this.queue.shift();
+                        if (nextFloor) {
+                            _this.move(nextFloor);
+                        }
+                    }, 2000);
+                }
+            });
+        }
+        else {
+            this.queue.push(floor);
+        }
+    };
+    // Function to animate the elevator's movement
+    Elevator.prototype.animateElevator = function (start, end, duration, callback) {
+        var _this = this;
+        var startTime = performance.now();
+        var animate = function (currentTime) {
+            var elapsedTime = currentTime - startTime;
+            var progress = Math.min(elapsedTime / duration, 1);
+            var newPosition = start + (end - start) * progress;
+            _this.elevatorElement.style.transform = "translateY(".concat(newPosition, "px)");
+            if (progress < 1) {
+                requestAnimationFrame(animate);
+            }
+            else {
+                callback();
+            }
+        };
+        requestAnimationFrame(animate);
+    };
+    Elevator.prototype.requestFloor = function (floor) {
+        if (!this.isWaiting) {
+            this.move(floor);
+        }
+        else {
+            this.queue.push(floor);
+        }
+    };
+    Elevator.prototype.playSound = function () {
+        var audio = new Audio('ding.mp3');
+        audio.play();
+        console.log('Ding!');
+    };
+    Elevator.prototype.updateElevatorPosition = function () {
+        var translateY = "calc(".concat(this.currentFloor.level, " * -57px)");
+        this.elevatorElement.style.transform = "translateY(".concat(translateY, ")");
+    };
+    return Elevator;
+}());
+var Building = /** @class */ (function () {
+    function Building(numberOfFloors, floorButtonsContainer) {
+        this.numberOfFloors = numberOfFloors;
+        this.floorButtonsContainer = floorButtonsContainer;
+    }
+    Building.prototype.createFloorButtons = function () {
+        var _loop_1 = function (i) {
+            var button = document.createElement('button');
+            button.classList.add('floor', 'metal', 'linear');
+            button.innerText = i.toString();
+            button.addEventListener('click', function () {
+                requestElevator(new Floor(i));
+            }.bind(this_1));
+            var div = document.createElement('div');
+            div.classList.add('blackline');
+            var floorDiv = document.createElement('div');
+            floorDiv.classList.add('floor');
+            floorDiv.appendChild(button);
+            this_1.floorButtonsContainer.appendChild(div);
+            this_1.floorButtonsContainer.appendChild(floorDiv);
+        };
+        var this_1 = this;
+        for (var i = this.numberOfFloors; i >= 0; i--) {
+            _loop_1(i);
+        }
+    };
+    return Building;
+}());
+var BuildingFactory = /** @class */ (function () {
+    function BuildingFactory() {
+    }
+    BuildingFactory.prototype.createBuilding = function (numberOfFloors, floorButtonsContainer) {
+        return new Building(numberOfFloors, floorButtonsContainer);
+    };
+    return BuildingFactory;
+}());
+var floorButtonsContainer = document.getElementById('floorButtonsContainer');
+var buildingFactory = new BuildingFactory();
+var elevator = null;
+function requestElevator(floor) {
+    if (!elevator) {
+        elevator = new Elevator(document.querySelector('.elevator img'));
+    }
+    elevator.requestFloor(floor);
+}
+var numberOfFloors = 15;
+var building = buildingFactory.createBuilding(numberOfFloors, floorButtonsContainer);
+building.createFloorButtons();
---
+++ index.ts
@@ -0,0 +1,288 @@
+
+// class Floor {
+//   level: number;
+
+//   constructor(level: number) {
+//     this.level = level;
+//   }
+// }
+
+// class Elevator {
+//   currentFloor: Floor;
+//   isMoving: boolean;
+//   queue: Floor[];
+//   isWaiting: boolean;
+//   elevatorElement: HTMLElement;
+
+//   constructor(element: HTMLElement) {
+//     this.currentFloor = new Floor(0);
+//     this.isMoving = false;
+//     this.queue = [];
+//     this.isWaiting = false;
+//     this.elevatorElement = element;
+//   }
+
+//   move(floor: Floor) {
+//     if (!this.isMoving) {
+//       this.isMoving = true;
+//       const travelDistance = Math.abs(this.currentFloor.level - floor.level); 
+//       const animationDuration = travelDistance * 0.5 * 1000; 
+//       this.elevatorElement.style.setProperty('--animation-duration', `${animationDuration}ms`);
+//       this.elevatorElement.style.setProperty('--target-floor', floor.level.toString());
+//       this.elevatorElement.style.setProperty('--current-floor', this.currentFloor.level.toString());
+
+//       setTimeout(() => {
+//         this.currentFloor = floor;
+//         console.log(`Elevator arrived at floor ${this.currentFloor.level}`);
+//         this.playSound();
+//         this.updateElevatorPosition();
+//         this.isMoving = false;
+//         if (this.queue.length > 0) {
+//           this.isWaiting = true;
+//           setTimeout(() => {
+//             this.isWaiting = false;
+//             const nextFloor = this.queue.shift();
+//             if (nextFloor) {
+//               this.move(nextFloor);
+//             }
+//           }, 2000); 
+//         }
+//       }, animationDuration);
+//     } else {
+//       this.queue.push(floor);
+//     }
+//     this.elevatorElement.style.setProperty('--target-floor', floor.level.toString());
+//     this.elevatorElement.style.setProperty('--current-floor', this.currentFloor.level.toString());
+//   }
+
+//   requestFloor(floor: Floor) {
+//     if (!this.isWaiting) {
+//       this.move(floor);
+//     } else {
+//       this.queue.push(floor);
+//     }
+//   }
+
+//   playSound() {
+//     const audio = new Audio('ding.mp3');
+//     audio.play();
+//     console.log('Ding!');
+//   }
+
+//   updateElevatorPosition() {
+//     const translateY = `calc(${this.currentFloor.level} * -57px)`;
+//     this.elevatorElement.style.transform = `translateY(${translateY})`;
+//   }
+// }
+
+// class Building {
+//   numberOfFloors: number;
+//   floorButtonsContainer: HTMLElement;
+
+//   constructor(numberOfFloors: number, floorButtonsContainer: HTMLElement) {
+//     this.numberOfFloors = numberOfFloors;
+//     this.floorButtonsContainer = floorButtonsContainer;
+//   }
+
+//   createFloorButtons() {
+//     for (let i = this.numberOfFloors; i >= 0; i--) {
+//       const button = document.createElement('button');
+//       button.classList.add('floor', 'metal', 'linear');
+//       button.innerText = i.toString();
+  
+//       button.addEventListener('click', function() {
+//         requestElevator(new Floor(i));
+//       }.bind(this));
+  
+//       const div = document.createElement('div');
+//       div.classList.add('blackline');
+  
+//       const floorDiv = document.createElement('div');
+//       floorDiv.classList.add('floor');
+//       floorDiv.appendChild(button);
+  
+//       this.floorButtonsContainer.appendChild(div);
+//       this.floorButtonsContainer.appendChild(floorDiv);
+//     }
+//   }
+
+// }
+
+// class BuildingFactory {
+//   createBuilding(numberOfFloors: number, floorButtonsContainer: HTMLElement) {
+//     return new Building(numberOfFloors, floorButtonsContainer);
+//   }
+// }
+
+// const floorButtonsContainer = document.getElementById('floorButtonsContainer');
+// const buildingFactory = new BuildingFactory();
+
+// let elevator: Elevator | null = null;
+
+// function requestElevator(floor: Floor) {
+//   if (!elevator) {
+//     elevator = new Elevator(document.querySelector('.elevator img')!);
+//   }
+//   elevator.requestFloor(floor);
+// }
+
+
+// const numberOfFloors = 15;
+// const building = buildingFactory.createBuilding(numberOfFloors, floorButtonsContainer!);
+// building.createFloorButtons();
+
+class Floor {
+  level: number;
+
+  constructor(level: number) {
+    this.level = level;
+  }
+}
+
+class Elevator {
+  currentFloor: Floor;
+  isMoving: boolean;
+  queue: Floor[];
+  isWaiting: boolean;
+  elevatorElement: HTMLElement;
+
+  constructor(element: HTMLElement) {
+    this.currentFloor = new Floor(0);
+    this.isMoving = false;
+    this.queue = [];
+    this.isWaiting = false;
+    this.elevatorElement = element;
+  }
+
+  move(floor: Floor) {
+    if (!this.isMoving) {
+      this.isMoving = true;
+      const currentY = parseInt(getComputedStyle(this.elevatorElement).getPropertyValue('transform').split(',')[5], 10);
+      const targetY = -57 * floor.level;
+
+      // Calculate the distance and duration of the animation
+      const distance = Math.abs(targetY - currentY);
+      const animationDuration = distance * 5 ;
+
+      // Ensure animationDuration is non-negative
+      const duration = Math.max(animationDuration, 0);
+
+      // Animate the elevator's movement
+      this.animateElevator(currentY, targetY, duration, () => {
+        this.currentFloor = floor;
+        console.log(`Elevator arrived at floor ${this.currentFloor.level}`);
+        this.playSound();
+        this.updateElevatorPosition();
+        this.isMoving = false;
+        if (this.queue.length > 0) {
+          this.isWaiting = true;
+          setTimeout(() => {
+            this.isWaiting = false;
+            const nextFloor = this.queue.shift();
+            if (nextFloor) {
+              this.move(nextFloor);
+            }
+          }, 2000);
+        }
+      });
+    } else {
+      this.queue.push(floor);
+    }
+  }
+
+  // Function to animate the elevator's movement
+  animateElevator(start: number, end: number, duration: number, callback: () => void) {
+    const startTime = performance.now();
+
+    const animate = (currentTime: number) => {
+      const elapsedTime = currentTime - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
+      const newPosition = start + (end - start) * progress;
+
+      this.elevatorElement.style.transform = `translateY(${newPosition}px)`;
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      } else {
+        callback();
+      }
+    };
+
+    requestAnimationFrame(animate);
+  }
+
+  requestFloor(floor: Floor) {
+    if (!this.isWaiting) {
+      this.move(floor);
+    } else {
+      this.queue.push(floor);
+    }
+  }
+
+  playSound() {
+    const audio = new Audio('ding.mp3');
+    audio.play();
+    console.log('Ding!');
+  }
+
+  updateElevatorPosition() {
+    const translateY = `calc(${this.currentFloor.level} * -57px)`;
+    this.elevatorElement.style.transform = `translateY(${translateY})`;
+  }
+}
+
+class Building {
+  numberOfFloors: number;
+  floorButtonsContainer: HTMLElement;
+
+  constructor(numberOfFloors: number, floorButtonsContainer: HTMLElement) {
+    this.numberOfFloors = numberOfFloors;
+    this.floorButtonsContainer = floorButtonsContainer;
+  }
+
+  createFloorButtons() {
+    for (let i = this.numberOfFloors; i >= 0; i--) {
+      const button = document.createElement('button');
+      button.classList.add('floor', 'metal', 'linear');
+      button.innerText = i.toString();
+  
+      button.addEventListener('click', function() {
+        requestElevator(new Floor(i));
+      }.bind(this));
+  
+      const div = document.createElement('div');
+      div.classList.add('blackline');
+  
+      const floorDiv = document.createElement('div');
+      floorDiv.classList.add('floor');
+      floorDiv.appendChild(button);
+  
+      this.floorButtonsContainer.appendChild(div);
+      this.floorButtonsContainer.appendChild(floorDiv);
+    }
+  }
+
+}
+
+class BuildingFactory {
+  createBuilding(numberOfFloors: number, floorButtonsContainer: HTMLElement) {
+    return new Building(numberOfFloors, floorButtonsContainer);
+  }
+}
+
+const floorButtonsContainer = document.getElementById('floorButtonsContainer');
+const buildingFactory = new BuildingFactory();
+
+let elevator: Elevator | null = null;
+
+function requestElevator(floor: Floor) {
+  if (!elevator) {
+    elevator = new Elevator(document.querySelector('.elevator img')!);
+  }
+  elevator.requestFloor(floor);
+}
+
+
+const numberOfFloors = 15;
+const building = buildingFactory.createBuilding(numberOfFloors, floorButtonsContainer!);
+building.createFloorButtons();
