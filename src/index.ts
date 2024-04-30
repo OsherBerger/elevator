476a33ea473a4d8589376e21f9270c43a324fc89
your-name <osherberger@gmail.com>
2024-04-30 17:35:09 +0300

    better readMe

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { BuildingFactory } from "./BuildingFactory/BuildingFactory";
 const buildingFactory = new BuildingFactory();
 const numberOfFloors = 15;
 const numberOfElevators = 3;
-const numberOfBuildings = 2;
+const numberOfBuildings = 3;
 
 for (let i = 0; i < numberOfBuildings; i++) {
   buildingFactory.createBuilding(numberOfFloors, numberOfElevators, i);
