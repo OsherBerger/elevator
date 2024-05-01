e7c6cbb8d51ff81546298eeace333fa039044de7
your-name <osherberger@gmail.com>
2024-05-01 11:21:47 +0300

    delete unneeded comments

--- src/BuildingFactory.ts
+++ src/BuildingFactory.ts
@@ -9,8 +9,8 @@ export class BuildingFactory {
   createBuilding(numberOfFloors: number, numberOfElevators: number, buildingIndex: number) {
     const container = document.createElement('div');
     container.classList.add('building');
-    container.style.width = `${numberOfElevators * 50}px`; // Adjust building width
-    container.style.marginLeft = `${buildingIndex * (numberOfElevators * 50 + 285)}px`; // Add margin to the building
+    container.style.width = `${numberOfElevators * 50}px`; 
+    container.style.marginLeft = `${buildingIndex * (numberOfElevators * 50 + 285)}px`; 
     document.getElementById('buildingsContainer')?.appendChild(container);
 
     return new Building(numberOfFloors, container, numberOfElevators);
