38b938243950118b7d1f9861a0d7f71b76933183
your-name <osherberger@gmail.com>
2024-05-06 00:42:47 +0300

    better wording for extra feature

--- src/App.ts
+++ src/App.ts
@@ -1,21 +1,17 @@
 /** app.ts
  * Initializes building objects using the BuildingFactory class.
  * Creates multiple buildings with a specified number of floors and elevators.
- */
+ * Gives an option for feature that adds background image dynamically
+*/
 
 import { BuildingFactory } from "./BuildingFactory";
 
-// Instantiate a BuildingFactory object
 const buildingFactory = new BuildingFactory();
 
-// Define the number of floors and elevators for each building
 const numberOfFloors = 7;
 const numberOfElevators = 3;
-
-// Define the number of buildings to create
 const numberOfBuildings = 1;
 
-// Create buildings using the BuildingFactory
 for (let i = 0; i < numberOfBuildings; i++) {
   buildingFactory.createBuilding(numberOfFloors, numberOfElevators, i);
 }
