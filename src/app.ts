53b808187827ba937d9f909402cb2ee48c2aefa3
your-name <osherberger@gmail.com>
2024-05-05 23:19:16 +0300

    refactor the app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,17 @@
 
 import { BuildingFactory } from "./BuildingFactory";
 
+// Instantiate a BuildingFactory object
 const buildingFactory = new BuildingFactory();
+
+// Define the number of floors and elevators for each building
 const numberOfFloors = 7;
 const numberOfElevators = 3;
+
+// Define the number of buildings to create
 const numberOfBuildings = 3;
 
+// Create buildings using the BuildingFactory
 for (let i = 0; i < numberOfBuildings; i++) {
   buildingFactory.createBuilding(numberOfFloors, numberOfElevators, i);
 }
